5dfd38c4350920eb1494c1749dcef792d315b844
Nathaniel Fagan <natervader13@gmail.com>
2026-06-07 16:08:51 -0400

    Rewrote my userChrome to pull from a git repo, concat each css file together

--- modules/home/n0ll.nix
+++ modules/home/n0ll.nix
@@ -71,7 +71,7 @@ in {
     services.mpdscribble.endpoints."last.fm" = {
       username = "natervader13";
       # TODO SOPS
-      # passwordFile = osConfig.sops.secrets."keys/n0ll/lastfm".path; 
+      passwordFile = osConfig.sops.secrets."keys/n0ll/lastfm".path; 
     };
 
 
@@ -95,10 +95,10 @@ in {
         # uses rycee flake for extensions, check available ones with `nix flake show "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"`
         sponsorblock
         # dearrow
-        # adnauseam
+        adnauseam
         vimium
-        ublock-origin
-        tridactyl
+        # ublock-origin
+        # tridactyl
         darkreader
         indie-wiki-buddy
         augmented-steam
@@ -115,242 +115,27 @@ in {
         # };
       };
 
-
-      userChrome = /*css*/ ''
-            /*
-            ┌─┐┬┌┬┐┌─┐┬  ┌─┐
-            └─┐││││├─┘│  ├┤
-            └─┘┴┴ ┴┴  ┴─┘└─┘
-            ┌─┐┌─┐─┐ ┬
-            ├┤ │ │┌┴┬┘
-            └  └─┘┴ └─
-
-            by Miguel Avila
-
-            */
-
-            /*
-
-            ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
-            │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
-            └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
-
-            */
-
-            :root {
-              --sfwindow: #19171a;
-              --sfsecondary: #201e21;
-            }
-
-            /* Urlbar View */
-
-            /*─────────────────────────────*/
-            /* Comment this section if you */
-            /* want to show the URL Bar    */
-            /*─────────────────────────────*/
-
-            .urlbarView {
-              display: none !important;
-            }
-
-            /*─────────────────────────────*/
-
-            /*
-            ┌─┐┌─┐┬  ┌─┐┬─┐┌─┐
-            │  │ ││  │ │├┬┘└─┐
-            └─┘└─┘┴─┘└─┘┴└─└─┘
-            */
-
-            /* Tabs colors  */
-            #tabbrowser-tabs:not([movingtab])
-              > #tabbrowser-arrowscrollbox
-              > .tabbrowser-tab
-              > .tab-stack
-              > .tab-background[multiselected='true'],
-            #tabbrowser-tabs:not([movingtab])
-              > #tabbrowser-arrowscrollbox
-              > .tabbrowser-tab
-              > .tab-stack
-              > .tab-background[selected='true'] {
-              background-image: none !important;
-              background-color: var(--toolbar-bgcolor) !important;
-                                }
-
-            /* Inactive tabs color */
-            #navigator-toolbox {
-              background-color: var(--sfwindow) !important;
-            }
-
-            /* Window colors  */
-            :root {
-              --toolbar-bgcolor: var(--sfsecondary) !important;
-              --tabs-border-color: var(--sfsecondary) !important;
-              --lwt-sidebar-background-color: var(--sfwindow) !important;
-              --lwt-toolbar-field-focus: var(--sfsecondary) !important;
-            }
-
-            /* Sidebar color  */
-            #sidebar-box,
-            .sidebar-placesTree {
-              background-color: var(--sfwindow) !important;
-            }
-
-            /*
-
-            ┌┬┐┌─┐┬  ┌─┐┌┬┐┌─┐
-             ││├┤ │  ├┤  │ ├┤
-            ─┴┘└─┘┴─┘└─┘ ┴ └─┘
-            ┌─┐┌─┐┌┬┐┌─┐┌─┐┌┐┌┌─┐┌┐┌┌┬┐┌─┐
-            │  │ ││││├─┘│ ││││├┤ │││ │ └─┐
-            └─┘└─┘┴ ┴┴  └─┘┘└┘└─┘┘└┘ ┴ └─┘
-
-            */
-
-            /* Tabs elements  */
-            .tab-close-button {
-              display: none;
-            }
-
-            /*
-            .tabbrowser-tab:not([pinned]) .tab-icon-image {
-              display: none !important;
-            }
-            */
-
-            #nav-bar:not([tabs-hidden='true']) {
-              box-shadow: none;
-            }
-
-            #tabbrowser-tabs[haspinnedtabs]:not([positionpinnedtabs])
-              > #tabbrowser-arrowscrollbox
-              > .tabbrowser-tab[first-visible-unpinned-tab] {
-              margin-inline-start: 0 !important;
-              }
-
-            :root {
-              --toolbarbutton-border-radius: 0 !important;
-              --tab-border-radius: 0 !important;
-              --tab-block-margin: 0 !important;
-            }
-
-            .tab-background {
-              border-right: 0px solid rgba(0, 0, 0, 0) !important;
-              margin-left: -4px !important;
-            }
-
-            .tabbrowser-tab:is([visuallyselected='true'], [multiselected])
-              > .tab-stack
-              > .tab-background {
-              box-shadow: none !important;
-              }
-
-            .tabbrowser-tab[last-visible-tab='true'] {
-              padding-inline-end: 0 !important;
-                            }
-
-            #tabs-newtab-button {
-              padding-left: 0 !important;
-            }
-
-            /* Url Bar  */
-            #urlbar-input-container {
-              background-color: var(--sfsecondary) !important;
-              border: 1px solid rgba(0, 0, 0, 0) !important;
-            }
-
-            #urlbar-container {
-              margin-left: 0 !important;
-            }
-
-            #urlbar[focused='true'] > #urlbar-background {
-              box-shadow: none !important;
-            }
-
-            #navigator-toolbox {
-              border: none !important;
-            }
-
-            /* Bookmarks bar  */
-            .bookmark-item .toolbarbutton-icon {
-              display: none;
-            }
-            toolbarbutton.bookmark-item:not(.subviewbutton) {
-              min-width: 1.6em;
-            }
-
-            /* Toolbar  */
-            #tracking-protection-icon-container,
-            #urlbar-zoom-button,
-            #star-button-box,
-            #pageActionButton,
-            #pageActionSeparator,
-            #tabs-newtab-button,
-            #back-button,
-            #PanelUI-button,
-            #forward-button,
-            .tab-secondary-label {
-              display: none !important;
-            }
-
-            .urlbarView-url {
-              color: #dedede !important;
-            }
-
-            /* Disable elements  */
-            #context-navigation,
-            #context-savepage,
-            #context-pocket,
-            #context-sendpagetodevice,
-            #context-selectall,
-            #context-viewsource,
-            #context-inspect-a11y,
-            #context-sendlinktodevice,
-            #context-openlinkinusercontext-menu,
-            #context-bookmarklink,
-            #context-savelink,
-            #context-savelinktopocket,
-            #context-sendlinktodevice,
-            #context-searchselect,
-            #context-sendimage,
-            #context-print-selection {
-              display: none !important;
-            }
-
-            #context_bookmarkTab,
-            #context_moveTabOptions,
-            #context_sendTabToDevice,
-            #context_reopenInContainer,
-            #context_selectAllTabs,
-            #context_closeTabOptions {
-              display: none !important;
-            }
-
-            /* Remove close button*/ .titlebar-buttonbox-container{ display:none }
-          '';
-
-      userContent = /* css */''
-            /*
-            ┌─┐┬┌┬┐┌─┐┬  ┌─┐
-            └─┐││││├─┘│  ├┤
-            └─┘┴┴ ┴┴  ┴─┘└─┘
-            ┌─┐┌─┐─┐ ┬
-            ├┤ │ │┌┴┬┘
-            └  └─┘┴ └─
-
-            by Miguel Avila
-
-            */
-
-            :root {
-              scrollbar-width: none !important;
-            }
-
-            @-moz-document url(about:privatebrowsing) {
-              :root {
-                scrollbar-width: none !important;
-              }
-            }
-       '';
+      userChrome = let
+        repo =  pkgs.fetchFromGitHub {
+          owner = "cascadefox";
+          repo = "cascade";
+          rev = "main";
+          hash = "sha256-MW6E9OaTGlnbHMRl8svgIyqd7BzYOjvUYi92sdgxNCc=";
+        };
+        file = pkgs.concatTextFile {
+          name = "userChrome.css";
+          files = lib.lists.map (x: repo + "/chrome/includes/cascade-" + x) [
+            "config.css"
+            "colours.css"
+            "layout.css"
+            "responsive.css"
+            "floating-panel.css"
+            "nav-bar.css"
+            "tabs.css"
+          ];
+        };
+      in builtins.readFile file;
+      
 
     };
 
