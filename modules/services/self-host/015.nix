eb51b9d47f9fed693da5561c353bf0838f127bb4
Nathaniel Fagan <natervader13@gmail.com>
2026-07-15 11:11:00 -0400

    Starting 015

---
+++ modules/services/self-host/015.nix
@@ -0,0 +1,52 @@
+{ self, inputs, config, ... }: {
+
+  flake.nixosModules.self-host = { lib, config, pkgs, ... }: let
+    mainDir = "/path/to/dir";
+
+    pull = "newer";
+    # worker and app use the same vols apparently
+    volumes = [
+      "${mainDir}/uploads:/uploads"
+      "${mainDir}/config.yaml:/config.yaml"
+    ];
+  in {
+
+    # ensure dirs are available for containers
+    system.activationScripts.pre-015.deps = [ "specialfs" ];
+    # system.activationScripts.pre-015.text = ''mkdir -p ${mainDir}/uploads ${mainDir}/config'';
+    system.activationScripts.pre-015.text = '' ${ lib.lists.map (x: "mkdir -p ${x}") volumes } '';
+
+    virtualisation.oci-containers.containers = {
+
+      "015-app" = {
+
+        image = "fudaoyuanicu/015-app";
+        inherit pull volumes;
+        ports = [ "31100:80" ];
+        dependsOn = [ "015-redis" ];
+
+      };
+
+      "015-worker" = {
+
+        inherit pull volumes;
+        image = "fudaoyuanicu/015-worker";
+        dependsOn = [
+          "015-app"
+          "015-redis"
+        ];
+
+      };
+
+      "015-redis" = {
+        inherit pull;
+        image = "redis:7";
+
+      };
+
+    };
+
+  };
+
+
+}
