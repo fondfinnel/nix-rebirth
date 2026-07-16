b09cf8f808e76a4a2ba97faf8c250da176be75c5
Nathaniel Fagan <natervader13@gmail.com>
2026-07-16 15:11:10 -0400

    Split disko preservation into it's own module

--- modules/hosts/nix-liquid.nix
+++ modules/hosts/nix-liquid.nix
@@ -55,114 +55,14 @@
     imports = [
 
       # declare partition scheme
-      self.nixosModules.nix-liquid-disko
+      self.nixosModules.disko-preservation
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480
 
     ];
 
+    disko.devices.disk.main.device = "/dev/sda";
+
   };  
 
-  flake.nixosModules.nix-liquid-disko = { pkgs, lib, config, ... }: {
-
-    imports = [
-      inputs.disko.nixosModules.disko
-    ];
-
-    preservation.enable = true;
-
-    fileSystems."/persist".neededForBoot = true;
-    fileSystems."/nix".neededForBoot = true;
-    # fileSystems."/etc/ssh".neededForBoot = true;
-    
-    
-    disko.devices.nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = [ "size=50%" "mode=755" ];
-    };
-
-    # mostly rewritten from https://haseebmajid.dev/posts/2024-07-30-how-i-setup-btrfs-and-luks-on-nixos-using-disko/
-    disko.devices.disk.main = {
-
-      # define the device itself
-      type = "disk";
-      device = "/dev/sda";
-
-      content = {
-
-        # set up the boot partition
-        type = "gpt";
-
-        partitions.boot = {
-          name = "boot";
-          size = "1M";
-          type = "EF02";
-        };
-
-        partitions.esp = {
-          name = "ESP";
-          size = "1G";
-          type = "EF00";
-
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
-        };
-
-        # define the root partition under luks encrypt
-        partitions.luks = {
-          
-          size = "100%";
-          label = "luks";
-
-          # luks part contains what?
-          # located in /dev/mapper
-          content = {
-            type = "luks";
-            name = "cryptroot";
-
-            extraOpenArgs = [
-              "--allow-discards"
-              "--perf-no_read_workqueue"
-              "--perf-no_write_workqueue"
-            ];
-
-            settings.crypttabExtraOpts = [
-              "fido2-device=auto"
-              "token-timeout=5"
-            ];
-
-            # the unencrypted data
-            content = let
-
-              # define shared mountOptions
-              n = [ "compress=zstd" "noatime" ];
-
-            in {
-              type = "btrfs";
-              extraArgs = [  "-f" ];
-
-              subvolumes = {
-
-                "/nix".mountpoint = "/nix";
-                "/nix".mountOptions = [ "subvol=nix" ] ++ n;
-
-                "/persist".mountpoint = "/persist";
-                "/persist".mountOptions = [ "subvol=persist" ] ++ n;
-
-              };
-
-            };
-
-          };
-
-        };
-
-      };
-
-    };
-
-  };
 
 }
