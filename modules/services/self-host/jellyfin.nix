132eb4c09757856a2fe3494efe2c8527c9a43836
Nathaniel Fagan <natervader13@gmail.com>
2026-08-19 15:43:16 -0400

    added ai upscaler, confirmed workikng now

--- modules/services/self-host/jellyfin.nix
+++ modules/services/self-host/jellyfin.nix
@@ -2,23 +2,31 @@
 { self, inputs, config, ... }: {
 
   flake.nixosModules.self-host = { lib, config, pkgs, ... }: let
-    mainDir = "/service/jellyfin";
+    mainDir = "/services/jellyfin";
   in {
  
     systemd.tmpfiles.rules = lib.map (f: "d ${f} 0755 1000 1000") [
       "${mainDir}"
       "${mainDir}/config"
       "${mainDir}/cache"
+      "${mainDir}/upscale-models"
     ];
 
 
+    # systemd.tmpfiles.rules = lib.map (f: "d ${f} 0755 root root") [
+    #   "/services/jellyfin-ai/models"
+    # ];
+
+    virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
+
     virtualisation.oci-containers.containers.jellyfin = {
-      image = "ghcr.io/jellyfin/jellyfin";
+      image = "docker.io/jellyfin/jellyfin";
       pull = "newer";
       ports = [
-        "127.0.0.1:31010:8096" 
+        "31010:8096" 
         "127.0.0.1:7359:7359" 
       ];
+
 
       devices = [
         "/dev/dri:/dev/dri"
@@ -28,7 +36,7 @@
       volumes = [
         "${mainDir}/config:/config" # redirect config storage
         "${mainDir}/cache:/cache" # redirect cache storage
-        "/home/n0ll/Videos:/media"
+        "/home/n0ll/Videos:/media:ro" # read only for media
       ];
 
       environment = {
@@ -38,6 +46,36 @@
         PUID = "1000";
         GUID = "1000";
       };
+
+    };
+
+    virtualisation.oci-containers.containers.jellyfin-ai-upscaler = {
+      # change branch depending on hw
+      image = "docker.io/kuscheltier/jellyfin-ai-upscaler:docker7-cpu";
+      pull = "newer";
+      ports = [
+        "31011:5000" 
+      ];
+
+      # devices = [ "/dev/dri" ];
+
+      volumes = [
+        "${mainDir}/upscale-models:/models" 
+      ];
+
+      # required for vulkan, intel arc
+      # extraOptions = [
+      #   "--group-add=render"
+      # ];
+
+      environment = {
+        LOG_LEVEL = "INFO";
+        TZ = config.time.timeZone;
+        PUID = "1000";
+        GUID = "1000";
+        API_TOKEN = "disable";
+      };
+
 
     };
 
