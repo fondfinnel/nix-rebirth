a161e9af527956c2f924af24c94cdcbc936581fe
Nathaniel Fagan <natervader13@gmail.com>
2026-09-19 14:14:09 -0400

    tailscale exit-node only for secondary

--- modules/services/common-utils/tailscale.nix
+++ modules/services/common-utils/tailscale.nix
@@ -11,12 +11,19 @@
 
       extraSetFlags = [ # Sets launch options for tailscaled
         "--accept-routes"
+
+        #server gets ssh support by default
       ] ++ lib.optionals (!config.headless-check) [
         "--ssh"
+
+        # in case another exit-node is used
       ] ++ lib.optionals (config.device-type == "primary") [ 
+        "--exit-node-allow-lan-access=true"
+
+        # secondary gets mullvad by default
+      ] ++ lib.optionals (config.device-type == "secondary") [
         "--exit-node=100.117.167.110" # For Mullvad, choosing which exit node to use. Needs to be an actual IP or else tailscaled-set complains at boot.
         # To find another mullvad node, run `tailscale exit-node <list/suggest>`
-        "--exit-node-allow-lan-access=true"
       ];
 
       useRoutingFeatures =
